356a4e98833004daf5a490d3dac38996062a058b
Kak Elay <Elay.Kak@amkcambodia.com>
2024-08-19 13:50:02 +0700

    after add Pagination

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,39 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../services/products.service';
-import { Products } from '../../types';
+import { Product, Products } from '../../types';
+import { ProductComponent } from '../components/product/product.component';
+import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
-  ngOnInit() {
+  products: Product[] = [];
+  totalRecord:number=0;
+  onProductOutput(product: Product) {
+    console.log(product, 'Output');
+  }
+  onPageChange(event:any){
+    this.fetchProducts(event.page, event.rows)
+  }
+  fetchProducts(page: number, perPage: number) {
     this.productsService
       .getProducts('http://localhost:3000/clothes', {
-        page: 0,
-        perPage: 5,
+        page,
+        perPage,
       })
-      .subscribe((products : Products) => {
-        console.log(products.items);
+      .subscribe((products: Products) => {
+        this.products = products.items;
+        this.totalRecord=products.total;
       });
+  }
+  ngOnInit() {
+    this.fetchProducts(0,5)
   }
 }
